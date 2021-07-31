9e28c5203c838769df2406a073e2dc5e0a9771fd
pepcerda <josep.cerda.roscar@gmail.com>
2021-07-31 10:36:04 +0200

    Creación proyecto spring

---
+++ backend-pesca-apirest/src/main/java/org/jcerdar/backend/apirest/BackendPescaApirestApplication.java
@@ -0,0 +1,13 @@
+package org.jcerdar.backend.apirest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendPescaApirestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendPescaApirestApplication.class, args);
+	}
+
+}
---
+++ backend-pesca-apirest/src/test/java/org/jcerdar/backend/apirest/BackendPescaApirestApplicationTests.java
@@ -0,0 +1,13 @@
+package org.jcerdar.backend.apirest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendPescaApirestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
